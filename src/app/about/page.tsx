87333d7d1c3fe9a6d0fc329dbd945b2d2b944d14
369webforall <369webforall@gmail.com>
2024-09-03 15:11:45 +0545

    t3app initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "~/components/ui/button";
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>This is About page</h1>
+      <Button>Click here to add....</Button>
+    </div>
+  );
+};
+
+export default AboutPage;
